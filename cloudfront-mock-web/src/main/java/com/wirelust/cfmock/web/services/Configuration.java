08f315bd91ab211fe4e8d1fafd7c21d358c34eeb
Terrence Curran <tcurran@approachingpi.com>
2016-06-18 01:19:48 -0400

    removed unused import

--- cloudfront-mock-web/src/main/java/com/wirelust/cfmock/web/services/Configuration.java
+++ cloudfront-mock-web/src/main/java/com/wirelust/cfmock/web/services/Configuration.java
@@ -2,7 +2,6 @@ package com.wirelust.cfmock.web.services;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.Serializable;
